6984f8a9122b8a1ebdb02e1b7f937d1f1f9cc26a
Nobuto Murata <nobuto.murata@canonical.com>
2021-10-08 14:12:08 +0900

    Initial Cookiecutter Commit.

---
+++ src/README.md
@@ -0,0 +1,19 @@
+IBMStorwizeSVC Storage Backend for Cinder
+-------------------------------
+
+Overview
+========
+
+This charm provides a IBMStorwizeSVC storage backend for use with the Cinder
+charm.
+
+To use:
+
+    juju deploy cinder
+    juju deploy cinder-ibmstorwizesvc
+    juju add-relation cinder-ibmstorwizesvc cinder
+
+Configuration
+=============
+
+See config.yaml for details of configuration options.
---
+++ src/lib/charm/openstack/cinder_ibmstorwizesvc.py
@@ -0,0 +1,37 @@
+import charms_openstack.charm
+import charmhelpers.core.hookenv as ch_hookenv  # noqa
+
+charms_openstack.charm.use_defaults('charm.default-select-release')
+
+
+class CinderIBMStorwizeSVCCharm(
+        charms_openstack.charm.CinderStoragePluginCharm):
+
+    # The name of the charm
+    name = 'cinder_ibmstorwizesvc'
+
+    # Package to determine application version. Use "cinder-common" when
+    # the driver is in-tree of Cinder upstream.
+    version_package = 'cinder-common'
+
+    # Package to determine OpenStack release name
+    release_pkg = 'cinder-common'
+
+    # this is the first release in which this charm works
+    release = 'ussuri'
+
+    # List of packages to install
+    packages = ['']
+
+    stateless = True
+
+    # Specify any config that the user *must* set.
+    mandatory_config = []
+
+    def cinder_configuration(self):
+        volume_driver = ''
+        driver_options = [
+            ('volume_driver', volume_driver),
+            # Add config options that needs setting on cinder.conf
+        ]
+        return driver_options
